b802b134619b930b6da8aaa45bc9678617f23206
aka-chukwu <kingjoda01@gmial.com>
2023-02-16 15:54:37 -0800

    love is good

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,16 @@
+/**
+ * main - Entry point for the program
+ *
+ * Description: Prints a message to standard error
+ * Return: Always 0 (Success)
+ */
+
 #include <stdio.h>
 
-int main() {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return 1;
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
+
 
